afa8ee707358d87439430b2f495318e6445ccc97
Natasha <75773163+natty-light@users.noreply.github.com>
2024-02-23 13:13:11 -0500

    Start implementing lexer, test for lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,173 @@
+package lexer
+
+import "QuonkScript/utils"
+
+const (
+	eqSym = '='
+
+	leftParen          = '('
+	rightParen         = ')'
+	leftSquareBracket  = '['
+	rightSquareBracket = ']'
+	leftCurlyBracket   = '{'
+	rightCurlyBracket  = '}'
+
+	semi  = ';'
+	comma = ','
+	colon = ':'
+	dot   = '.'
+
+	addSym  = '+'
+	multSym = '*'
+	divSym  = '/'
+	subSym  = '-'
+	modSym  = '%'
+
+	greaterThan = '>'
+	lessThan    = '<'
+	bang        = '!'
+	ampersand   = '&'
+	pipe        = '|'
+)
+
+type Lexer struct {
+	source       string
+	position     int
+	readPosition int
+	char         byte
+	line         int
+}
+
+func CreateLexer(source string) *Lexer {
+	lexer := &Lexer{source: source, line: 1} // Start our lexer at line 1
+	lexer.readChar()                         // set up lexer
+	return lexer
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.source) {
+		l.char = 0
+	} else {
+		l.char = l.source[l.readPosition]
+	}
+
+	// if we read in a newline or carriage return, we should reset the column counter
+	if l.char == '\n' || l.char == '\r' {
+		l.line += 1
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) readIdentifer() string {
+	position := l.position
+
+	for utils.IsAlpha(string(l.char)) {
+		l.readChar() // Advances the position pointer
+	}
+	return l.source[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for utils.IsNumeric(string(l.char)) {
+		l.readChar() // This just advances the position pointer
+	}
+	return l.source[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.char == ' ' || l.char == '\t' || l.char == '\n' || l.char == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) NextToken() Token {
+	var tok Token
+	l.skipWhitespace()
+	switch l.char {
+	// Assignment
+	case eqSym:
+		tok = token(Assign, l.char, l.line)
+
+	// grouping
+	case leftParen:
+		tok = token(LeftParen, l.char, l.line)
+	case rightParen:
+		tok = token(RightParen, l.char, l.line)
+	case leftCurlyBracket:
+		tok = token(LeftCurlyBracket, l.char, l.line)
+	case rightCurlyBracket:
+		tok = token(RightCurlyBracket, l.char, l.line)
+	case leftSquareBracket:
+		tok = token(RightSquareBracket, l.char, l.line)
+	case rightSquareBracket:
+		tok = token(RightSquareBracket, l.char, l.line)
+
+	// Punctuation
+	case semi:
+		tok = token(Semicolon, l.char, l.line)
+	case comma:
+		tok = token(Comma, l.char, l.line)
+	case colon:
+		tok = token(Colon, l.char, l.line)
+	case dot:
+		tok = token(Dot, l.char, l.line)
+
+	// Operators
+	case addSym:
+		tok = token(Plus, l.char, l.line)
+	case subSym:
+		tok = token(Minus, l.char, l.line)
+	case multSym:
+		tok = token(Product, l.char, l.line)
+	case divSym:
+		tok = token(Quotient, l.char, l.line)
+	case modSym:
+		tok = token(Modulo, l.char, l.line)
+
+		// TODO: Logical operators
+	case 0:
+		tok.Literal = ""
+		tok.Type = "EOF"
+
+	default:
+		if utils.IsAlpha(string(l.char)) {
+			tok.Literal = l.readIdentifer()
+			tok.Type = LookupIdent(tok.Literal)
+			tok.Line = l.line
+			return tok // This is to avoid the l.readChar() call before this functions return
+		} else if utils.IsNumeric(string(l.char)) {
+			tok.Type = Int
+			tok.Literal = l.readNumber()
+			tok.Line = l.line
+			return tok // This is to avoid the l.readChar() call before this functions return
+		} else {
+			tok = token(Illegal, l.char, l.line)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+var keywords = map[string]TokenType{
+	"mut":    Mut,
+	"const":  Const,
+	"null":   Null,
+	"true":   True,
+	"false":  False,
+	"if":     If,
+	"else":   Else,
+	"elseif": Elseif,
+	"func":   Func,
+	"return": Return,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return Identifier
+}
---
+++ lexer/lexer_test.go
@@ -0,0 +1,80 @@
+package lexer
+
+import "testing"
+
+func TestNextToken(t *testing.T) {
+	source := `mut five = 5;
+	mut ten = 10;
+	mut add = func (x, y) {
+		 x + y;
+	};
+	mut result = add(five, ten);`
+
+	tests := []struct {
+		expectedType    TokenType
+		expectedLiteral string
+		expectedLine    int
+	}{
+		{Mut, "mut", 1},
+		{Identifier, "five", 1},
+		{Assign, "=", 1},
+		{Int, "5", 1},
+		{Semicolon, ";", 1},
+
+		{Mut, "mut", 2},
+		{Identifier, "ten", 2},
+		{Assign, "=", 2},
+		{Int, "10", 2},
+		{Semicolon, ";", 2},
+
+		{Mut, "mut", 3},
+		{Identifier, "add", 3},
+		{Assign, "=", 3},
+		{Func, "func", 3},
+		{LeftParen, "(", 3},
+		{Identifier, "x", 3},
+		{Comma, ",", 3},
+		{Identifier, "y", 3},
+		{RightParen, ")", 3},
+		{LeftCurlyBracket, "{", 3},
+
+		{Identifier, "x", 4},
+		{Plus, "+", 4},
+		{Identifier, "y", 4},
+		{Semicolon, ";", 4},
+
+		{RightCurlyBracket, "}", 5},
+		{Semicolon, ";", 5},
+
+		{Mut, "mut", 6},
+		{Identifier, "result", 6},
+		{Assign, "=", 6},
+		{Identifier, "add", 6},
+		{LeftParen, "(", 6},
+		{Identifier, "five", 6},
+		{Comma, ",", 6},
+		{Identifier, "ten", 6},
+		{RightParen, ")", 6},
+		{Semicolon, ";", 6},
+
+		{EOF, "", 0},
+	}
+
+	lexer := CreateLexer(source)
+
+	for i, tt := range tests {
+		tok := lexer.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+
+		if tok.Line != tt.expectedLine {
+			t.Fatalf("tests[%d] - line wrong. expected=%d, got=%d", i, tt.expectedLine, tok.Line)
+		}
+	}
+
+}
