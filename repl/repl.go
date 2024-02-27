e2bf31c8e4743bdf0fb3cf107a3e3002b77a80d6
Natasha <75773163+natty-light@users.noreply.github.com>
2024-02-26 19:41:55 -0500

    Improve REPL

--- repl/repl.go
+++ repl/repl.go
@@ -2,7 +2,7 @@ package repl
 
 import (
 	"QuonkScript/lexer"
-	"QuonkScript/token"
+	"QuonkScript/parser"
 	"bufio"
 	"fmt"
 	"io"
@@ -12,7 +12,7 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
+	fmt.Fprint(out, "QuonkScript REPL v0.1\n")
 	for {
 		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
@@ -23,9 +23,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
