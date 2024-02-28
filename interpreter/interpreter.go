d2352f6b5ff6a826ef88152d7c8e81b5586f65d7
Natasha <75773163+natty-light@users.noreply.github.com>
2024-02-28 17:21:30 -0500

    Add support for running files

---
+++ interpreter/interpreter.go
@@ -0,0 +1,29 @@
+package interpreter
+
+import (
+	"QuonkScript/evaluator"
+	"QuonkScript/lexer"
+	"QuonkScript/object"
+	"QuonkScript/parser"
+	"fmt"
+	"os"
+)
+
+func Run(filename string) {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Honk! Cannot read file %s\n", filename)
+		return
+	}
+
+	src := string(bytes)
+
+	l := lexer.New(src)
+	p := parser.New(l)
+	scope := object.NewScope()
+
+	program := p.ParseProgram()
+	// parser.PrintAST(prog)
+	result := evaluator.Eval(program, scope)
+	fmt.Println(result)
+}
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"QuonkScript/interpreter"
 	"QuonkScript/repl"
 	"os"
 )
 
 func main() {
-	repl.Start(os.Stdin, os.Stdout)
+
+	args := os.Args
+
+	if len(args) == 1 {
+		// If no filename was passed as a command line argument, run the repl
+		repl.Start(os.Stdin, os.Stdout)
+	} else {
+		// Script name should be second arg
+		interpreter.Run(args[1])
+	}
+
 }
