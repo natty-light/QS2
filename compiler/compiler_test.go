1200e401a3cab7751693c39f34620823fa2510b4
Natasha <75773163+natty-light@users.noreply.github.com>
2024-03-10 13:56:46 -0400

    Comparisons

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -108,106 +108,106 @@ func TestBooleanExpressions(t *testing.T) {
 				code.Make(code.OpPop),
 			},
 		},
-		// {
-		// 	source:            "1 > 2",
-		// 	expectedConstants: []interface{}{1, 2},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpConstant, 0),
-		// 		code.Make(code.OpConstant, 1),
-		// 		code.Make(code.OpGt),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "1 < 2",
-		// 	expectedConstants: []interface{}{1, 2},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpConstant, 0),
-		// 		code.Make(code.OpConstant, 1),
-		// 		code.Make(code.OpGt),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "1 >= 2",
-		// 	expectedConstants: []interface{}{1, 2},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpConstant, 0),
-		// 		code.Make(code.OpConstant, 1),
-		// 		code.Make(code.OpGte),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "1 <= 2",
-		// 	expectedConstants: []interface{}{1, 2},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpConstant, 0),
-		// 		code.Make(code.OpConstant, 1),
-		// 		code.Make(code.OpGte),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "1 == 2",
-		// 	expectedConstants: []interface{}{1, 2},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpConstant, 0),
-		// 		code.Make(code.OpConstant, 1),
-		// 		code.Make(code.OpEqual),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "1 != 2",
-		// 	expectedConstants: []interface{}{1, 2},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpConstant, 0),
-		// 		code.Make(code.OpConstant, 1),
-		// 		code.Make(code.OpNotEqual),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "true == false",
-		// 	expectedConstants: []interface{}{},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpTrue),
-		// 		code.Make(code.OpFalse),
-		// 		code.Make(code.OpEqual),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "true != false",
-		// 	expectedConstants: []interface{}{},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpTrue),
-		// 		code.Make(code.OpFalse),
-		// 		code.Make(code.OpNotEqual),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "true && false",
-		// 	expectedConstants: []interface{}{},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpTrue),
-		// 		code.Make(code.OpFalse),
-		// 		code.Make(code.OpAnd),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
-		// {
-		// 	source:            "true || false",
-		// 	expectedConstants: []interface{}{},
-		// 	expectedInstructions: []code.Instructions{
-		// 		code.Make(code.OpTrue),
-		// 		code.Make(code.OpFalse),
-		// 		code.Make(code.OpOr),
-		// 		code.Make(code.OpPop),
-		// 	},
-		// },
+		{
+			source:            "1 > 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpGt),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "1 < 2",
+			expectedConstants: []interface{}{2, 1},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpGt),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "1 >= 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpGte),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "1 <= 2",
+			expectedConstants: []interface{}{2, 1},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpGte),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "1 == 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpEqual),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "1 != 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpNotEqual),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "true == false",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpTrue),
+				code.Make(code.OpFalse),
+				code.Make(code.OpEqual),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "true != false",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpTrue),
+				code.Make(code.OpFalse),
+				code.Make(code.OpNotEqual),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "true && false",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpTrue),
+				code.Make(code.OpFalse),
+				code.Make(code.OpAnd),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			source:            "true || false",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpTrue),
+				code.Make(code.OpFalse),
+				code.Make(code.OpOr),
+				code.Make(code.OpPop),
+			},
+		},
 	}
 
 	runCompilerTests(t, tests)
