5eb753e3c6590acf9b3a8a324bf2cf00eda92162
Natasha <75773163+natty-light@users.noreply.github.com>
2024-03-21 16:28:04 -0400

    Add global and local scope to symbol table

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -4,6 +4,7 @@ type SymbolScopes string
 
 const (
 	GlobalScope SymbolScopes = "GLOBAL"
+	LocalScope  SymbolScopes = "LOCAL"
 )
 
 type Symbol struct {
@@ -14,6 +15,8 @@ type Symbol struct {
 }
 
 type SymbolTable struct {
+	Outer *SymbolTable
+
 	store          map[string]Symbol
 	numDefinitions int
 }
@@ -24,14 +27,26 @@ func NewSymbolTable() *SymbolTable {
 }
 
 func (s *SymbolTable) DefineMutable(name string) Symbol {
-	symbol := Symbol{Name: name, Index: s.numDefinitions, Scope: GlobalScope, IsConstant: false}
+	symbol := Symbol{Name: name, Index: s.numDefinitions, IsConstant: false}
+	if s.Outer == nil {
+		symbol.Scope = GlobalScope
+	} else {
+		symbol.Scope = LocalScope
+	}
+
 	s.store[name] = symbol
 	s.numDefinitions++
 	return symbol
 }
 
 func (s *SymbolTable) DefineImmutable(name string) Symbol {
-	symbol := Symbol{Name: name, Index: s.numDefinitions, Scope: GlobalScope, IsConstant: true}
+	symbol := Symbol{Name: name, Index: s.numDefinitions, IsConstant: true}
+	if s.Outer == nil {
+		symbol.Scope = GlobalScope
+	} else {
+		symbol.Scope = LocalScope
+	}
+
 	s.store[name] = symbol
 	s.numDefinitions++
 	return symbol
@@ -39,5 +54,14 @@ func (s *SymbolTable) DefineImmutable(name string) Symbol {
 
 func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
 	symbol, ok := s.store[name]
+	if !ok && s.Outer != nil {
+		symbol, ok = s.Outer.Resolve(name)
+	}
 	return symbol, ok
+}
+
+func NewEnclosedSymbolTable(outer *SymbolTable) *SymbolTable {
+	s := NewSymbolTable()
+	s.Outer = outer
+	return s
 }
--- compiler/symbol_table_test.go
+++ compiler/symbol_table_test.go
@@ -6,6 +6,10 @@ func TestDefine(t *testing.T) {
 	expected := map[string]Symbol{
 		"a": {Name: "a", Scope: "GLOBAL", Index: 0, IsConstant: true},
 		"b": {Name: "b", Scope: "GLOBAL", Index: 1, IsConstant: false},
+		"c": {Name: "c", Scope: "LOCAL", Index: 0, IsConstant: true},
+		"d": {Name: "d", Scope: "LOCAL", Index: 1, IsConstant: false},
+		"e": {Name: "e", Scope: "LOCAL", Index: 0, IsConstant: true},
+		"f": {Name: "f", Scope: "LOCAL", Index: 1, IsConstant: false},
 	}
 
 	globalScope := NewSymbolTable()
@@ -18,6 +22,28 @@ func TestDefine(t *testing.T) {
 	b := globalScope.DefineMutable("b")
 	if b != expected["b"] {
 		t.Errorf("b = %v, expected %v", b, expected["b"])
+	}
+
+	firstLocal := NewEnclosedSymbolTable(globalScope)
+	c := firstLocal.DefineImmutable("c")
+	if c != expected["c"] {
+		t.Errorf("c = %v, expected %v", c, expected["c"])
+	}
+
+	d := firstLocal.DefineMutable("d")
+	if d != expected["d"] {
+		t.Errorf("d = %v, expected %v", d, expected["d"])
+	}
+
+	secondLocal := NewEnclosedSymbolTable(firstLocal)
+	e := secondLocal.DefineImmutable("e")
+	if e != expected["e"] {
+		t.Errorf("e = %v, expected %v", e, expected["e"])
+	}
+
+	f := secondLocal.DefineMutable("f")
+	if f != expected["f"] {
+		t.Errorf("f = %v, expected %v", f, expected["f"])
 	}
 
 }
@@ -39,7 +65,86 @@ func TestResolveGlobal(t *testing.T) {
 		}
 
 		if result != sym {
-			t.Errorf("result = %v, expected %v", result, sym)
+			t.Errorf("expected %s to resolve to %+v, got=%+v", sym.Name, sym, result)
+		}
+	}
+}
+
+func TestResolveNestedLocal(t *testing.T) {
+	globalScope := NewSymbolTable()
+	globalScope.DefineImmutable("a")
+	globalScope.DefineMutable("b")
+
+	firstLocal := NewEnclosedSymbolTable(globalScope)
+	firstLocal.DefineImmutable("c")
+	firstLocal.DefineMutable("d")
+
+	secondLocal := NewEnclosedSymbolTable(firstLocal)
+	secondLocal.DefineImmutable("e")
+	secondLocal.DefineMutable("f")
+
+	tests := []struct {
+		table           *SymbolTable
+		expectedSymbols []Symbol
+	}{
+		{
+			firstLocal,
+			[]Symbol{
+				{Name: "a", Scope: GlobalScope, Index: 0, IsConstant: true},
+				{Name: "b", Scope: GlobalScope, Index: 1, IsConstant: false},
+				{Name: "c", Scope: LocalScope, Index: 0, IsConstant: true},
+				{Name: "d", Scope: LocalScope, Index: 1, IsConstant: false},
+			},
+		},
+		{
+			secondLocal,
+			[]Symbol{
+				{Name: "a", Scope: GlobalScope, Index: 0, IsConstant: true},
+				{Name: "b", Scope: GlobalScope, Index: 1, IsConstant: false},
+				{Name: "e", Scope: LocalScope, Index: 0, IsConstant: true},
+				{Name: "f", Scope: LocalScope, Index: 1, IsConstant: false},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		for _, sym := range tt.expectedSymbols {
+			result, ok := tt.table.Resolve(sym.Name)
+			if !ok {
+				t.Errorf("name %s not resolvable", sym.Name)
+			}
+
+			if result != sym {
+				t.Errorf("expected %s to resolve to %+v, got=%+v", sym.Name, sym, result)
+			}
+		}
+	}
+}
+
+func TestResolveLocal(t *testing.T) {
+	globalScope := NewSymbolTable()
+	globalScope.DefineImmutable("a")
+	globalScope.DefineMutable("b")
+
+	local := NewEnclosedSymbolTable(globalScope)
+	local.DefineImmutable("c")
+	local.DefineMutable("d")
+
+	expected := []Symbol{
+		{Name: "a", Scope: GlobalScope, Index: 0, IsConstant: true},
+		{Name: "b", Scope: GlobalScope, Index: 1, IsConstant: false},
+		{Name: "c", Scope: LocalScope, Index: 0, IsConstant: true},
+		{Name: "d", Scope: LocalScope, Index: 1, IsConstant: false},
+	}
+
+	for _, sym := range expected {
+		result, ok := local.Resolve(sym.Name)
+		if !ok {
+			t.Errorf("name %s not resolvable", sym.Name)
+		}
+
+		if result != sym {
+			t.Errorf("expected %s to resolve to %+v, got=%+v", sym.Name, sym, result)
 		}
 	}
 }
