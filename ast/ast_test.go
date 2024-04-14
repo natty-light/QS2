5fe77f3494f8885c247a24739deab81a2105264a
Natasha <75773163+natty-light@users.noreply.github.com>
2024-04-14 13:07:49 -0400

    Start fixing tests for types

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -11,13 +11,14 @@ func TestString(t *testing.T) {
 			&VarDeclarationStmt{
 				Token:    token.Token{Type: token.Mut, Literal: "mut"},
 				Name:     &Identifier{Token: token.Token{Type: token.Identifier, Literal: "x"}, Value: "x"},
+				VarType:  &TypeLiteral{Token: token.Token{Type: token.IntType, Literal: "int"}},
 				Value:    &Identifier{Token: token.Token{Type: token.Identifier, Literal: "y"}, Value: "y"},
 				Constant: false,
 			},
 		},
 	}
 
-	if program.String() != "mut x = y;" {
+	if program.String() != "mut x int = y;" {
 		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
